31de2d1e5ba9a22b3e9649b21070e87cd04e35b2
stanleychow <chowtech1022@gmail.com>
2019-09-02 19:03:56 -0700

    Add files via upload
    First rough draft of the Mad Libs Project script.

---
+++ mad_libs.py
@@ -0,0 +1,5 @@
+
+
+Result = "There was once a " + noun_1 + " and their pet " + adj_1 + " " + noun_2 + ". They woke up one morning to find out a " + adj_2 + " " + noun_3 + " had " + verb + " all over their " + noun_4 + ". In response, they took ou their trusty " + adj_3 + " " + noun_5 + " 3000 to " + verb_2 + " it up!"
+
+print(Result)
